c29b05e39e53beb716a7e751590ec0c1c88ac8e2
GIassWindows <90355174+GIassWindows@users.noreply.github.com>
2022-02-16 05:49:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,31 +10,29 @@ class hotel{
         var i, len, text // variables for the loop
         for(i = 0, len = north.length + south.length, text = "";i<len; i++){
             text += "We have hotels in " + northAndSouth[i] + "." + "\n"; 
-        }
-        return text;
-    }
+        } // for every time len is greater than i(0), loop the text until len is no longer greater than i
+        return text; // prints the text after looping
+    } 
 
-    static northSouth(a){
-        if (a == "North"||a == "north"){ // if this.direction is North or north
+    static northSouth(direction){
+        if (direction == "North"||direction == "north"){ // if direction is North or north
             var i, len, text // variables for the loop
             for(i = 0, len = north.length, text = "";i<len; i++){
                 text += "We have a hotel in " + north[i] + "." + "\n";
             } // for every time len is greater than i(0), loop the text until len is no longer greater than i
             return text; // prints the text after looping
-        }else if (a == "South"||a == "south"){ // if this.direction is South or south
+        }else if (direction == "South"||direction == "south"){ // if direction is South or south
             var i, len, text
             for(i = 0, len = south.length, text = "";i<len; i++){
                 text += "We have a hotel in " + south[i] + "." + "\n";
             } // for every time len is greater than i(0), loop until len is no longer greater than i
             return text; // prints the text after looping
         }else {
-            return a + " is not a valid direction." /* if this.direction is neither North, north, South, or south,  
+            return direction + " is not a valid direction." /* if this.direction is neither North, north, South, or south,  
             prints that it's not a valid location */
         }
     }
 }
-
-
 
 var direction = readline.question('Where do you want to go? ') // using the module, readline asks the question in the terminal
 console.log(hotel.allHotels()) // outputs the allHotels function
